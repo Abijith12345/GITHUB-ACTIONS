2a263117c28a261b62e72ea07a9287d9dc689c31
abijith <habijith2002@gmail.com>
2024-11-27 20:05:48 +0530

    {}

--- IAM.tf
+++ IAM.tf
@@ -8,8 +8,8 @@ data "aws_ssm_parameter" "secret_key" {
 }
 
 provider "aws" {
-  access_key = data.aws_ssm_parameter.access_key.value
-  secret_key = data.aws_ssm_parameter.secret_key.value
+  access_key = "${data.aws_ssm_parameter.access_key.value}"
+  secret_key = "${data.aws_ssm_parameter.secret_key.value}"
   region ="us-east-1"
 }
 
